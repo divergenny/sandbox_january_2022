0c82d2d5f4ebdbfe72cc76508358f90a996003ed
divergenny <divergenny@gmail.com>
2023-04-22 23:12:37 +0500

    initial commit

---
+++ sandbox-web/src/main/java/com/exadel/sandbox/team2/controller/UserRestController.java
@@ -0,0 +1,53 @@
+package com.exadel.sandbox.team2.controller;
+
+import com.exadel.sandbox.team2.domain.User;
+import com.exadel.sandbox.team2.dto.UserDto;
+import com.exadel.sandbox.team2.mapper.UserMapper;
+import com.exadel.sandbox.team2.serivce.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.NotFoundException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserRestController {
+
+    private final UserService service;
+
+    private final UserMapper mapper;
+
+    @GetMapping("/{id}")
+    public UserDto findById(@PathVariable Long id) {
+        Optional<User> user = service.findById(id);
+        return user.map(mapper::toDto)
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @SneakyThrows
+    @GetMapping
+    public List<UserDto> findAll() {
+        List<User> list = service.findAll();
+        return list.stream().map(mapper::toDto).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public UserDto add(@RequestBody UserDto userDto) {
+        return service.save(userDto);
+    }
+
+    @PutMapping("/{id}")
+    public UserDto update(@PathVariable Long id, @RequestBody UserDto userDto) {
+        return service.update(userDto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
